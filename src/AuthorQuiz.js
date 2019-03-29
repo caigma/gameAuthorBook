7286a524c2b2200205945c00c9010617c0b93c7b
caigma <caigma@gmail.com>
2019-03-29 14:16:20 +0000

    selection answer a change color if it is correct o worng

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
-import EvenCounter from './EvenCounter';
+// import EvenCounter from './EvenCounter';
+import PropTypes from 'prop-types';
 
 function Hero() {
 	return (
@@ -15,57 +16,74 @@ function Hero() {
 	);
 }
 
-function Book({title}) {
-  return (<div className="answer">
-    <h4>{title}</h4>
-  </div>
-  );
+function Book({ title, onClick }) {
+	return (
+		<div
+			className="answer"
+			onClick={() => {
+				onClick(title);
+			}}
+		>
+			<h4>{title}</h4>
+		</div>
+	);
 }
 
-function Turn({author, books}) {
-  return (
-  
-  <div className="row turn" style={{backgroundColor: "white"}}>
-    <div className="col-4 offset-1">
-      <img src={author.imageUrl} className="authorimage" alt="Author"/>
-    </div>
-    <div className="col-6">
-      {books.map((title) => 
-     
-      <Book title={title} key={title} />
-      
-      )}
-    </div>
+function Turn({ author, books, highlight, onAnswerSelected }) {
+	function highlightToBgColor(highlight) {
+		const mapping = {
+			none: '',
+			correct: 'green',
+			wrong: 'red'
+		};
+		return mapping[highlight];
+	}
+
+  return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+  <div className="col-4 offset-1">
+    <img src={author.imageUrl} className="authorimage" alt="Author"/>
   </div>
-  );
+  <div className="col-6">
+    {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+  </div>
+</div>);
 }
+
+Turn.propTypes = {
+author: PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  imageSource: PropTypes.string.isRequired,
+  books: PropTypes.arrayOf(PropTypes.string).isRequired
+}),
+books: PropTypes.arrayOf(PropTypes.string).isRequired,
+onAnswerSelected: PropTypes.func.isRequired,
+highlight: PropTypes.string.isRequired
+};
 
 function Continue() {
-  return (<div/>);
+return (<div/>);
 }
 
 function Footer() {
-  return (<div id="footer" className="row">
-    <div className="col-12">
-        <p className="text-muted credit">
-          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
-        </p>
-    </div>
-  </div>);
+return (<div id="footer" className="row">
+  <div className="col-12">
+      <p className="text-muted credit">
+        All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+      </p>
+  </div>
+</div>);
 }
 
-
-
-function AuthorQuiz({turnData}) {
-  return (
-    <div className="container-fluid">
-      <Hero />
-      <Turn {...turnData} />
-      <Continue />
-      <Footer />
-      <EvenCounter onEvenClick={(data)=> {console.log(`even ${data}`);}} />
-    </div>
-  );
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+return (
+  <div className="container-fluid">
+    <Hero />
+    <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+    <Continue />
+    <Footer />
+  </div>
+);
 }
 
 export default AuthorQuiz;
